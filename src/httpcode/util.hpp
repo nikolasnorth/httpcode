8c002d9d796c77fe8a2b3d01d68b620752e9988a
nikolasnorth <nikolasnorth1@gmail.com>
2022-06-06 15:22:05 -0400

    Add comments to functions
    Comments were added to the `to_digit()` and `format_output()` functions to describe their behaviour.

--- src/httpcode/util.hpp
+++ src/httpcode/util.hpp
@@ -7,8 +7,11 @@
 
 namespace httpcode {
 
+// Tries to parse the given digit string into an integer.
 tl::expected<int, std::exception> to_digit(std::string_view s) noexcept;
 
+// Generates an output message to display information about the given HTTP
+// status code.
 std::string format_output(unsigned int code, std::string_view short_desc,
                           std::string_view long_desc,
                           std::string_view learn_more_url);
